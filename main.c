83f625a6aeb982b24794eb5397f42584656b8c30
ismail0ka <ismail.bouchaib1@protonmail.com>
2023-04-17 13:32:46 +0000

    simple Shell

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
+#include "simple_shell.h"
 
 int main(int argc,char** argv){
+    //Load any config files
     shell_loop();
+    //Perform any cleanup (&& ||) shutdown 
     return EXIT_SUCCESS;
 }
--- simple_shell.h
+++ simple_shell.h
@@ -2,6 +2,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 void shell_loop(void){
     char* line;// the command
@@ -10,7 +13,7 @@ void shell_loop(void){
     do{
         printf("$ ");
         line = shell_read_line();//read the command
-        args = shell_split_line();//split the arguments
+        args = shell_split_line(line);//split the arguments
         status = shell_execute(args);//execute the command
 
         free(line);
@@ -52,6 +55,7 @@ char* shell_read_line(void){
     }
 }
 
+/* #Another version of the read line function using getline()
 char* shell_get_line(void){
     char* line = NULL;
     ssize_t buffer_size = 0;
@@ -65,4 +69,129 @@ char* shell_get_line(void){
         }
     }
     return line;
+}*/
+
+#define SH_TOK_BUFFER 64
+#define SH_TOK_DELIM " \t\r\n\a"
+char** shell_split_line(char* line){
+    int buffer_size = SH_TOK_BUFFER, position = 0;
+    char **tokens = (char**) malloc(buffer_size * sizeof(char*));
+    char* token;
+
+    if(!tokens){
+        fprintf(stderr,"sh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, SH_TOK_DELIM);// tokenize the command into name and arguments
+    while(token != NULL){
+        tokens[position] = token;
+        position++;
+        if(position >= buffer_size){
+            buffer_size += SH_TOK_BUFFER;
+            tokens = (char**) realloc(tokens,buffer_size * sizeof(char*));
+            if(!tokens){
+                fprintf(stderr,"sh: allocation error \n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, SH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;//the list of args
+}
+
+int shell_launch(char **args){
+    pid_t pid, wpid;
+    int status;
+    pid = fork();
+    if(pid == 0){
+        //Child process
+        if(execvp(args[0],args) == -1){
+            perror("sh");
+        }
+        exit(EXIT_FAILURE);
+    }else if(pid < 0){
+        //Error forking to a process
+        perror("sh error forking");
+    }else{
+        //Parent process
+        do{
+            wpid = waitpid(pid, &status, WUNTRACED);
+        }while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
+/*
+    Declaration of built-in functions
+*/
+int sh_cd(char** args);
+int sh_help(char** args);
+int sh_exit(char** args);
+
+/*
+    List of built-in commands
+*/  
+char* builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+/*
+    Pointer to the list of built-in functions that return an int and take a list of strings as an argument
+*/
+int (*builtin_func[]) (char**) = {
+    &sh_cd,
+    &sh_help,
+    &sh_exit
+};
+
+/*
+    return the number of built-in commands in list
+*/
+int sh_num_builtins(){
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+    Built-in funcs implementation
+*/
+int sh_cd(char** args){
+    if(args[1] == NULL){
+        fprintf(stderr,"sh: expected argument to (cd) \n");
+    }else{
+        if(chdir(args[1]) != 0){
+            perror("No such file or repo");
+        }
+    }
+    return 1;
+}
+
+int sh_help(char** args){
+    int i;
+    printf("Simple shell:\n");
+    printf("type your command in the form (cmd_name cmd_arg) then hit enter");
+    printf("The list of built_in commands");
+    for(i=0;i<sh_num_builtins();i++){
+        printf("-%d: %s \n",i+1,builtin_str[i]);
+    }
+    printf("for further commands use the (man) cmd");
+}
+
+int sh_exit(char **args){
+    return 0;
+}
+
+int sh_execute(char** args){
+    int i;
+    if(args[0] == NULL){
+        return 1;
+    }
+    for(i=0;i<sh_num_builtins();i++){
+        if(strcmp(args[0],builtin_str[i]) == 0){
+            return (*builtin_func[i])(args);
+        }
+    }
+    return shell_launch(args);
 }
